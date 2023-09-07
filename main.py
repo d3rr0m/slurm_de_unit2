f48c3f598cd958ee97395cc2b5c238727839a295
Roman Deriga <deriga.roman@gmail.com>
2023-09-07 15:24:41 +0300

    Add requests.session

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import requests
 import pprint
+from tqdm import tqdm
 
 
 def main():
     url = 'http://5.159.103.105:4000/api/v1/logs'
     params = {
-        'page': 3
+        'page': 1
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
     response_json = response.json()
     max_page = round(response_json['totalEntries']/200+1)
-    with open('file.json', 'a') as file:
-        for page in range(1, 100+1):
+    content = []
+    with requests.session() as session:
+        for page in tqdm(range(1, max_page), colour='BLUE', desc='Fetching data outside loop:'):
             params['page'] = page
-            response = requests.get(url, params=params)
+            response = session.get(url, params=params)
             response.raise_for_status()
-            response_json = response.json()
-            file.write(str(response_json))
+            content.append(response.json())
+
+    with open('file.json', 'a') as file:
+        file.write(content)
 
 
 if __name__ == '__main__':
