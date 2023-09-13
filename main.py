ef9f33ce2e17339a4c373ff579398ad8c07f908d
deriga roman <deriga.roman@gmail.com>
2023-09-13 09:26:59 +0000

    HTTP client change to aiohttp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from collections.abc import Callable, Iterable, Mapping
 from typing import Any
-import requests
+import aiohttp
+from aiohttp import ClientSession
+import asyncio
 import urllib
 from collections import namedtuple
 from urllib.parse import urlunparse, urlencode
@@ -9,33 +11,22 @@ from threading import Thread
 import time
 
 
+
 URL = 'http://5.159.103.105:4000/api/v1/logs'
 #MAX_PAGE = 131962
-MAX_PAGE = 13100
+MAX_PAGE = 1310
 result = []
 
 
-class DownloadThread(Thread):
-
-    def __init__(self, queue, name):
-        super().__init__()
-        self.queue = queue
-        self.name = name
-
-
-    def run(self):
-        while True:
-            url = self.queue.get()
-            response = requests.get(url)
-            #response.raise_for_status()
-            if response.status_code != 200:
-                print(f'==============={response.status_code}==={self.name}')
-            #print(f'starts {url}')
-            #response_json = response.json()
-            result.append(response.json())
-            self.queue.task_done()
-            #with open('file.json', 'a') as file:
-            #    file.write(str(response.json()))
+async def download_page(session: ClientSession, url: str):
+    async with session.get(url) as response:
+        return response.status
+    #response.raise_for_status()
+    #if response.status_code != 200:
+        #print(f'starts {url}')
+        #response_json = response.json()
+        #with open('file.json', 'a') as file:
+        #    file.write(str(response.json()))
 
 
 def create_url(page):
@@ -59,24 +50,16 @@ def create_url(page):
     return url
 
 
-def main():
+async def main():
     urls = [create_url(page) for page in range(1, MAX_PAGE)]
-    q = queue.Queue()
-    threads = [DownloadThread(q, f'Thread {i+1}') for i in range(500)]
 
-    for t in threads:
-        t.daemon = True
-        t.start()
-
-    for url in urls:
-        q.put(url)
-
-    q.join()
-    with open('file.json', 'a') as file:
-        file.write(str(result))
+    async with aiohttp.ClientSession() as session:
+        status = await download_page(session, urls[0])
+        print(f'{status} status had been returned')
 
 
 if __name__ == '__main__':
     start_time = time.time()
-    main()
+    asyncio.run(main())
     print("--- %s seconds ---" % (time.time() - start_time))
+
